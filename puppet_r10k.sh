c4a7b2dc2ea9b630992a26a2ec0d177dbc59fa62
Martin Schinz <m.schinz@gmail.com>
2014-12-19 10:35:10 +0000

    tidy up script

--- puppet_r10k.sh
+++ puppet_r10k.sh
@@ -13,12 +13,14 @@ fi
 apt-get update >/dev/null
 
 echo "Installing ruby 1.9.3..."
-apt-get install -y ruby1.9.1 ruby1.9.1-dev \
-	rubygems1.9.1 irb1.9.1 ri1.9.1 rdoc1.9.1 \
+apt-get install -y  \
+	 \
 	build-essential libopenssl-ruby1.9.1 libssl-dev zlib1g-dev >/dev/null
 
-#update-alternatives --set ruby /usr/bin/ruby1.9.1
-#update-alternatives --set gem /usr/bin/gem1.9.1
+DEBIAN_FRONTEND=noninteractive apt-get -y \
+	-o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
+	install ruby1.9.1 ruby1.9.1-dev rubygems1.9.1 irb1.9.1 ri1.9.1 rdoc1.9.1 \
+	build-essential libopenssl-ruby1.9.1 libssl-dev zlib1g-dev >/dev/null
 
 echo "Installing git..."
 apt-get install -y git >/dev/null
