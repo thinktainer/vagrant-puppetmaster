6038412cf2c33b2208259373a9d718ceebebda7f
Martin Schinz <m.schinz@gmail.com>
2014-12-19 10:01:52 +0000

    replace puppet bootstrappers with hashicorp's
    - downloaded from [hashicorp
      repo](https://github.com/hashicorp/puppet-bootstrap)

--- puppet_master.sh
+++ puppet_master.sh
@@ -1,10 +1,20 @@
-#!/usr/bin/env bash
-#
-# This bootstraps Puppet on Ubuntu 12.04 LTS.
-#
+#!/usr/bin/env sh
+
+# This bootstraps Puppet on Debian
 set -e
 
-REPO_DEB_URL="http://apt.puppetlabs.com/puppetlabs-release-precise.deb"
+# Do the initial apt-get update
+echo "Initial apt-get update..."
+apt-get update >/dev/null
+
+# Older versions of Debian don't have lsb_release by default, so 
+# install that if we have to.
+which lsb_release || apt-get install -y lsb-release
+
+# Load up the release information
+DISTRIB_CODENAME=$(lsb_release -c -s)
+
+REPO_DEB_URL="http://apt.puppetlabs.com/puppetlabs-release-${DISTRIB_CODENAME}.deb"
 
 #--------------------------------------------------------------------
 # NO TUNABLES BELOW THIS POINT
@@ -14,15 +24,19 @@ if [ "$EUID" -ne "0" ]; then
   exit 1
 fi
 
+# Install wget if we have to (some older Debian versions)
+echo "Installing wget..."
+apt-get install -y wget >/dev/null
+
 # Install the PuppetLabs repo
 echo "Configuring PuppetLabs repo..."
 repo_deb_path=$(mktemp)
-wget --output-document=${repo_deb_path} ${REPO_DEB_URL} 2>/dev/null
-dpkg -i ${repo_deb_path} >/dev/null
+wget --output-document="${repo_deb_path}" "${REPO_DEB_URL}" 2>/dev/null
+dpkg -i "${repo_deb_path}" >/dev/null
 apt-get update >/dev/null
 
 # Install Puppet
 echo "Installing Puppet..."
-apt-get install -y puppet >/dev/null
+DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install puppet >/dev/null
 
 echo "Puppet installed!"
